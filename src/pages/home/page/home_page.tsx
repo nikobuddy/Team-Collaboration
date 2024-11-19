f926713e08da2014bd34d80073ea5113a71a1852
Gayatri Wadgem <gayatrivadge@gmail.com>
2024-11-19 23:29:52 +0530

    feat(ui):added responsive navigation bar for better user experience

--- src/pages/home/page/home_page.tsx
+++ src/pages/home/page/home_page.tsx
@@ -1,10 +1,8 @@
-
 const HomePage = () => {
-  return <div>
+  return <div className="p-10 bg-[#FF0000FF]">
     <h1 className="text-2xl font-bold">Welcome to Bike Builders!</h1>
     <p className="mt-2">This is your dashboard.</p>
   </div>;
 };
 
 export default HomePage;
-
