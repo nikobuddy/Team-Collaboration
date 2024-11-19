e5c982d0da3e67167f0635a7b7a994498127d4c8
Gayatri Wadgem <gayatrivadge@gmail.com>
2024-11-19 23:47:12 +0530

    fix(ui):resolved overlapping issue in the footer on smaller screens

--- src/pages/home/page/home_page.tsx
+++ src/pages/home/page/home_page.tsx
@@ -2,7 +2,11 @@ const HomePage = () => {
   return <div className="p-10 bg-[#FF0000FF]">
     <h1 className="text-2xl font-bold">Welcome to Bike Builders!</h1>
     <p className="mt-2">This is your dashboard.</p>
+    <img src="C:\Users\Harshada jadhav\Desktop" alt="Team" />
+    <p className="mt-2">This is your dashboard.</p>
   </div>;
+
+  
 };
 
 export default HomePage;
