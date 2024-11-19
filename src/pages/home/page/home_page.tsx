3aac9bc673d904e9a0e30ce4b8f267b503a49be4
Gayatri Wadgem <gayatrivadge@gmail.com>
2024-11-20 00:06:21 +0530

    feat(ui/ux):enhanced button hover effects for improved interactivity

--- src/pages/home/page/home_page.tsx
+++ src/pages/home/page/home_page.tsx
@@ -1,5 +1,5 @@
 const HomePage = () => {
-  return <div className="p-10 bg-[#FF0000FF]">
+  return <div>
     <h1 className="text-2xl font-bold">Welcome to Bike Builders!</h1>
     <p className="mt-2">This is your dashboard.</p>
     <img src="C:\Users\Harshada jadhav\Desktop" alt="Team" />
